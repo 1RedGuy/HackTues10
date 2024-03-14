64194dfd16bfe3760290f7952cfe9f9e9bf912f4
1RedGuy <119105124+1RedGuy@users.noreply.github.com>
2024-03-14 11:23:04 +0200

    Profiles Post Route

--- backend/index.py
+++ backend/index.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from utils.decorators import HandleResponse, ValidateRequest, ValidateSignUp, Create, SignUpAccess
+from utils.decorators import HandleResponse, ValidateRequest, ValidateSignUp, Create, SignUpAccess, VerifyRole, VerifyToken
 from dotenv import load_dotenv
 from utils.functions.token import generate_token
 from database.index import get_by_val
@@ -15,20 +15,16 @@ from utils.decorators import HandleResponse
 app = Flask(__name__)
 
 @app.route('/')
-
 def home():
     return "Home"
 
 
 @app.route("/info", methods=["GET"])
-
 @HandleResponse
 def info():
     return can_sign_up(), 200
 
 @app.route('/sign-up', methods=['POST'])
-
-
 @HandleResponse
 @SignUpAccess
 @ValidateRequest
@@ -38,6 +34,14 @@ def sign_up():
     id = GetByModel("profile").id
     return generate_token({"profileId": id}), 200
 
+@app.route("/profiles", methods=["POST"])
+@HandleResponse
+@ValidateRequest
+@VerifyToken
+@VerifyRole("admin")
+@Create("profile")
+def create_profile():
+    return True, 201
 
 if __name__ == '__main__':
     app.run(debug=True)
