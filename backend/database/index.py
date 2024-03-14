88e35ab9786852c6b170d2cf5de6714d32a78b76
Gosho69 <127147526+Gosho69@users.noreply.github.com>
2024-03-14 09:08:27 +0200

    create_new_record function added

--- backend/database/index.py
+++ backend/database/index.py
@@ -4,5 +4,23 @@ from database.dbmodels import Profile, Subject, Students_to_Subject, ProfileSche
 
 engine = create_engine("mysql+mysqld://guest@%:123456@localhost:3306/db_name")
 
+models = {"profile": Profile, "subject":Subject, "student_to_subject":Students_to_Subject}
+schemas = {"profile": ProfileSchema, "subject": SubjectSchema}
+
 def create_new_record(model_name, model_dict):
-    pass
+    with Session(engine) as session:
+        models[model_name].__table__.create(bind=engine, checkfirst=True)
+        new_record = models[model_name](**model_dict)
+
+        session.add(new_record)
+        session.commit()
+    
+    return schemas[model_name]().dump(new_record)
+
+def get_by_val(model_name, by, val):
+    with Session(engine) as session:
+         models[model_name].__table__.create(bind=engine, checkfirst=True)
+
+         stmt = select(models[model_name]).where(getattr(models[model_name], by) == val)
+
+
